00d770937f1b85fa742cdc967e2db7b44c249523
conejitomaya <eduardobonillac@gmail.com>
2020-02-06 09:38:27 -0600

    nokogiri html parsed added

--- main.rb
+++ main.rb
@@ -4,6 +4,8 @@
 
 require 'json'
 require 'rest-client'
+require 'nokogiri'
+require 'http'
 
 def cls
   system clear
@@ -11,17 +13,21 @@ end
 
 def ask
   string = gets.chomp
-  string.split(' ').join('+')
+  asked = string.to_s.split(' ').join('+')
+  asked
 end
 
 def search(query)
   url = "https://www.bing.com/search?q=#{query}"
   response = RestClient.get(url)
-  # parsed = JSON.parse(response)
+  parsed = Nokogiri::HTML.parse(response.body)
 
   if response.code.eql?(200)
     puts 'The page is responding... OK - STATUS'
     puts 'The parsed page info is:'
+    parsed.css("a li ol").each do |content|
+      puts "\tsearch:#{content.text}\tlink:#{content[:href]}\n"
+    end
 
   else
     puts 'The page is not responding... STATUS - FAILURE'
